49ecb4890552e142ae353e46e8c3af3d707778c5
Mark Westcott <mark@markw.de>
2016-02-24 14:33:47 +0100

    Add debug example

---
+++ economics/teaching/ss2016/R/other/debug-example.R
@@ -0,0 +1,16 @@
+
+rm(list = ls())
+
+
+sqrtPlusFour <- function(z) {
+  y <- z + 4
+  sqrt(y)
+}
+
+doSomething <- function(a) {
+  if (a == 3) {
+    print("a is 3")
+  } else {
+    sqrtPlusFour(a)
+  }
+}
